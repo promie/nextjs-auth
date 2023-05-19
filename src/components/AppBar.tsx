f37234dcfebc05ba72610c5b63ed72f0b9776213
Promie Yutasane <promie.yutasane@gmail.com>
2023-05-20 09:35:47 +1000

    NEXT-17 constructed the AppBar component

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import Link from 'next/link'
+
+const AppBar: FC = () => {
+  return (
+    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+      <Link className="transition-colors hover:text-blue-500" href="/">
+        Home Page
+      </Link>
+      <Link className="transition-colors hover:text-blue-500" href="/user-post">
+        User Post Page
+      </Link>
+    </header>
+  )
+}
+
+export default AppBar
