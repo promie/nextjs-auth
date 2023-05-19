3c2a6d8c8910bbc711aabc79d9d6e5b6be865d64
Promie Yutasane <promie.yutasane@gmail.com>
2023-05-20 08:26:00 +1000

    NEXT-16 configure daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,20 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  // daisyUI config (optional)
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#38b6ff',
+          secondary: '#f6d860',
+          accent: '#FF0000',
+          neutral: '#CFCFCF',
+          'base-100': '#ffffff',
+        },
+      },
+      'light',
+    ],
+  },
 }
