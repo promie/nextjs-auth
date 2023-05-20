6a974b42bad3688e2c5ee9c354d7f6c625723976
Promie Yutasane <promie.yutasane@gmail.com>
2023-05-20 19:13:43 +1000

    NEXT-22 protected the /id/post route with nextjs

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,9 +1,23 @@
 import prisma from '@lib/prisma'
+import { verifyJwt } from '@lib/jwt'
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
+  const accessToken = request.headers.get('authorization')
+
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(
+      JSON.stringify({
+        error: 'unauthorized',
+      }),
+      {
+        status: 401,
+      },
+    )
+  }
+
   const userPosts = await prisma.post.findMany({
     where: { authorId: +params.id },
     include: {
